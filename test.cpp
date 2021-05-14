d9a11f2df8b06d2ee80eaf1ccce53e8ef636e684
SJLEE <lee.sangjun101@gmail.com>
2021-05-14 23:51:24 +0900

    test.cpp

--- test.cpp
+++ test.cpp
@@ -5,4 +5,6 @@ int main(){
     std::cout<<"Hello World!"<< std::endl;
     std::cout<<"NOW"<<std::endl;
     std::cout<<"ERROR"<<std::endl;
+    std::cout<<"ERROR"<<std::endl;
+    std::cout<<"ERROR"<<std::endl;
 }
